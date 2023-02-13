6f68084c6f5a81dffd488d7f292c15bd6a6124b8
bewe <16521486@mahasiswa.itb.ac.id>
2023-02-13 22:37:17 +0700

    feat: add components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
 import getConfig from 'next/config';
 import Image from 'next/image';
-import ImageTes from '@src/assets/images/tes.png'
+
+import ImageTes0 from '@src/assets/images/under-construction.svg'
+import ImageTes1 from '@src/assets/backgrounds/background-titik.svg'
+import ImageTes2 from '@src/assets/backgrounds/background-matahari.svg'
+import ImageTes5 from '@src/assets/backgrounds/layer tes.png'
+import ImageTes3 from '@src/assets/images/biru-kiri-bawah.svg'
+import ImageTes4 from '@src/assets/images/biru-kanan-bawah.svg'
+import ImageTes6 from '@src/assets/images/bola-biru.svg'
+import ImageTes7 from '@src/assets/images/bola-ijo.svg'
+import ImageTes8 from '@src/assets/images/benteng.svg'
+import ImageTes9 from '@src/assets/images/gajah.svg'
+import ImageTes10 from '@src/assets/images/pion-kuning.svg'
+import ImageTes11 from '@src/assets/images/pion-pink.svg'
+import ImageTes12 from '@src/assets/images/dadu-ungu.svg'
+import ImageTes13 from '@src/assets/images/dadu-biru.svg'
+import ImageTes14 from '@src/assets/images/ular-merah.svg'
+import ImageTes15 from '@src/assets/images/ular-ungu.svg'
+import ImageTes16 from '@src/assets/images/ular-kuning.svg'
+import ImageTes17 from '@src/assets/images/pita1.svg'
+import ImageTes18 from '@src/assets/images/pita2.svg'
+import ImageTes19 from '@src/assets/images/tangga.svg'
+import ImageTes20 from '@src/assets/images/papan.svg'
+import ImageTes21 from '@src/assets/images/kipas.svg'
 import Layout from '@src/components/Navigation/Layout';
 
 const { publicRuntimeConfig } = getConfig();
@@ -9,116 +31,77 @@ const { name } = publicRuntimeConfig.site;
 const Home = () => {
   return (
     <Layout>
-    <div>
-    <Image src={ImageTes} />
-    </div>
-      <section className="py-12">
-        <div className="container mx-auto px-4">
-          <div className="prose prose-blue mx-auto">
-            <h1>{name}</h1>
-            <p>Welcome to my Next.js + Tailwind CSS starter template.</p>
-
-            <h2>Features:</h2>
-            <ul>
-              <li>
-                Pre-configured <code>postcss.config.js</code> and{' '}
-                <code>tailwind.config.js</code>.
-              </li>
-              <li>
-                Markdown and CMS Content friendly with{' '}
-                <code>@tailwindcss/typography</code>.
-              </li>
-              <li>
-                Form styles with <code>@tailwindcss/forms</code>.
-              </li>
-              <li>
-                <code>global.css</code> that contains Tailwind CSS directives.
-              </li>
-              <li>
-                <code>{'<Layout />'}</code> component.
-              </li>
-            </ul>
-
-            <h2>Guides</h2>
-            <ul>
-              <li>
-                <a
-                  href="https://nextjs.org/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Next.js
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://tailwindcss.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Tailwind CSS
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/tailwindlabs/tailwindcss-typography"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @tailwindcss/typography
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/tailwindlabs/tailwindcss-forms"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @tailwindcss/forms
-                </a>
-              </li>
-            </ul>
-
-            <h2>Form control examples</h2>
-            <form action="" className="mb-0 space-y-6">
-              <div>
-                <label
-                  htmlFor="sample-email"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Sample email input
-                </label>
-                <input
-                  type="email"
-                  name="sample-email"
-                  id="sample-email"
-                  placeholder="Enter your email address"
-                />
-              </div>
-              <div className="flex items-center">
-                <label htmlFor="sample-checkbox">
-                  <input
-                    type="checkbox"
-                    name="sample-checkbox"
-                    id="sample-checkbox"
-                  />
-                  <span className="ml-2">A sample checkbox</span>
-                </label>
-              </div>
-              <div className="flex items-center space-x-4">
-                <label htmlFor="sample-radio-1">
-                  <input type="radio" name="sample-radio" id="sample-radio-1" />
-                  <span className="ml-2">A sample radio number one</span>
-                </label>
-                <label htmlFor="sample-radio-2">
-                  <input type="radio" name="sample-radio" id="sample-radio-2" />
-                  <span className="ml-2">A sample radio number two</span>
-                </label>
-              </div>
-            </form>
-          </div>
+      <div className="bg-gradient-to-r from-white to-[#913175] relative">
+        <div className="w-max">
+          <Image src={ImageTes0} />
         </div>
-      </section>
+        <div className="font-[Marker-Felt] text-[#674188] font-medium text-[15px]">
+          We’re currently working on something glorious, we’ll be launching soon.
+        </div>
+        <div className="font-[Heavy-Heap] text-[#674188] text-[25px]">
+          Roll your dice, be the ace.
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes2} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes3} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes4} />
+        </div>
+        <div className="w-full">
+          <Image src={ImageTes5} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes6} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes7} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes8} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes9} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes10} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes11} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes12} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes13} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes14} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes15} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes16} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes17} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes18} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes19} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes20} />
+        </div>
+        <div className="w-[200px]">
+          <Image src={ImageTes21} />
+        </div>
+      </div>
     </Layout>
   );
 };
