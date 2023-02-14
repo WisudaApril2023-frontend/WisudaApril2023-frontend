4706a0c973bacb0d5ecbb5ffa2fb1ffb88f97a57
bewe <16521486@mahasiswa.itb.ac.id>
2023-02-14 17:26:31 +0700

    feat: add halfdone coming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import getConfig from 'next/config';
 import Image from 'next/image';
 
-import ImageTes0 from '@src/assets/images/under-construction.svg'
+import title from '@src/assets/images/under-construction.svg'
 import ImageTes1 from '@src/assets/backgrounds/background-titik.svg'
-import ImageTes2 from '@src/assets/backgrounds/background-matahari.svg'
-import ImageTes5 from '@src/assets/backgrounds/layer tes.png'
-import ImageTes3 from '@src/assets/images/biru-kiri-bawah.svg'
-import ImageTes4 from '@src/assets/images/biru-kanan-bawah.svg'
+import matahari from '@src/assets/backgrounds/background-matahari.svg'
+import layer from '@src/assets/backgrounds/layer tes.png'
+import blueObjectLeft from '@src/assets/images/biru-kiri-bawah.svg'
+import blueObjectRight from '@src/assets/images/biru-kanan-bawah.svg'
 import ImageTes6 from '@src/assets/images/bola-biru.svg'
 import ImageTes7 from '@src/assets/images/bola-ijo.svg'
 import ImageTes8 from '@src/assets/images/benteng.svg'
@@ -25,85 +25,43 @@ import ImageTes20 from '@src/assets/images/papan.svg'
 import ImageTes21 from '@src/assets/images/kipas.svg'
 import Layout from '@src/components/Navigation/Layout';
 
-const { publicRuntimeConfig } = getConfig();
-const { name } = publicRuntimeConfig.site;
-
-const Home = () => {
+const ComingSoon = (): JSX.Element => {
   return (
-    <Layout>
-      <div className="bg-gradient-to-r from-white to-[#913175] relative">
-        <div className="w-max">
-          <Image src={ImageTes0} />
-        </div>
-        <div className="font-[Marker-Felt] text-[#674188] font-medium text-[15px]">
-          We’re currently working on something glorious, we’ll be launching soon.
-        </div>
-        <div className="font-[Heavy-Heap] text-[#674188] text-[25px]">
-          Roll your dice, be the ace.
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes2} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes3} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes4} />
-        </div>
-        <div className="w-full">
-          <Image src={ImageTes5} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes6} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes7} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes8} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes9} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes10} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes11} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes12} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes13} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes14} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes15} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes16} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes17} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes18} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes19} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes20} />
-        </div>
-        <div className="w-[200px]">
-          <Image src={ImageTes21} />
+    <div className="bg-gradient-to-r from-white to-[#913175] h-screen overflow-hidden">
+      <div className="mt-[100px] ml-[100px]">
+        <Image src={title} />
+        <div className="w-1/5">
+          <div className="font-[Marker-Felt] text-[#674188] font-medium text-[18px] mt-[10px]">
+            We’re currently working on something glorious, we’ll be launching soon.
+          </div>
+          <div className="font-[Heavy-Heap] text-[#674188] text-[30px]">
+            Roll your dice, be the ace.
+          </div>
         </div>
       </div>
-    </Layout>
-  );
-};
+      <div className="right-0 absolute top-[-2px] h-screen overflow-hidden z-10">
+        <Image src={layer} />
+      </div>
+        <div className="w-[350px] left-0 bottom-0 absolute z-10 overflow-hidden">
+          <Image src={blueObjectLeft} />
+        </div>
+      <div className="w-screen place-items-center grid z-0">
+        <div className="w-[700px] bottom-0 absolute">
+          <Image src={matahari} />
+        </div>
+      </div>
+      <div className="w-[350px] right-0 bottom-0 absolute z-10">
+        <Image src={blueObjectRight} />
+      </div>
+      
+      {/* <div className="w-[500px] pt-[436px] absolute">
+        <Image src={matahari} />
+      </div> */}
+      {/* <div className="w-[400px] pt-[440px] pl-[100px] absolute">
+        <Image src={blueObjectRight} />
+      </div> */}
+    </div>
+  )
+}
 
-export default Home;
+export default ComingSoon
