bbfb88ad9fbcb38b347733d0a86143cfcec9acad
bewe <16521486@mahasiswa.itb.ac.id>
2023-03-17 18:32:06 +0700

    feat: merge Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 // import UnderConstruction from '@src/contexts/UnderConstruction/UnderConstruction'
-import Jumbotron from '@src/contexts/Homepage/Jumbotron'
 import confetti from '@src/assets/backgrounds/confetti.svg'
+
+import Jumbotron from '@src/contexts/Homepage/Jumbotron'
+import Footer from '@src/components/Navigation/Footer'
+
 
 const Home = (): JSX.Element => {
   const backgroundImageStyle = {
@@ -9,10 +12,11 @@ const Home = (): JSX.Element => {
     backgroundSize: '1130px 1000px'
   }
   return (
-    <div className="bg-[#8C4975] h-auto relative overflow-x-hidden" style={backgroundImageStyle}>
+    <div className="bg-[#8C4975] h-[1000px] relative overflow-x-hidden" style={backgroundImageStyle}>
       <div className='h-screen relative'>
         <Jumbotron />
       </div>
+      <Footer />
     </div>
   )
 }
