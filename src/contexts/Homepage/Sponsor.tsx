bd2947e9b5fbda6e04063a2384e7a1f3055e2afa
bewe <16521486@mahasiswa.itb.ac.id>
2023-03-20 09:49:15 +0700

    feat: add sponsors

--- src/contexts/Homepage/Sponsor.tsx
+++ src/contexts/Homepage/Sponsor.tsx
@@ -1,64 +1,93 @@
 import Image from 'next/image'
 import kipas from '@src/assets/backgrounds/kipasmatahari.svg'
-// import matahari from '@src/assets/backgrounds/background-matahari-cropped.svg'
-import acarakita from '@src/assets/images/acarakita-logo.svg'
-import skillUp from '@src/assets/images/skillup-logo.svg'
-import aisec from '@src/assets/images/aiesec-logo.svg'
-import tribun from '@src/assets/images/tribunjabar-logo.svg'
-import inilah from '@src/assets/images/logo-inilah.svg'
-import parah from '@src/assets/images/parah-logo.svg'
-import infobandung from '@src/assets/images/infobandung-logo.svg'
+
+import formula from '@src/assets/images/sponsornmedpar/ry-logo-without-glow-rgb-01-1-copy.svg'
+import fullo from '@src/assets/images/sponsornmedpar/fullo-black-logo-1.svg'
+import macito from '@src/assets/images/sponsornmedpar/logo-macito-png-2.svg'
+import mintz from '@src/assets/images/sponsornmedpar/logo-mintz-soft-candy-1.svg'
+import tango from '@src/assets/images/sponsornmedpar/logo-tango-2021-01-1.svg'
+
+import acarakita from '@src/assets/images/sponsornmedpar/acarakita-logo.svg'
+import skillUp from '@src/assets/images/sponsornmedpar/skillup-logo.svg'
+import aisec from '@src/assets/images/sponsornmedpar/aiesec-logo.svg'
+import tribun from '@src/assets/images/sponsornmedpar/tribunjabar-logo.svg'
+import inilah from '@src/assets/images/sponsornmedpar/logo-inilah.svg'
+import parah from '@src/assets/images/sponsornmedpar/parah-logo.svg'
+import infobandung from '@src/assets/images/sponsornmedpar/infobandung-logo.svg'
 
 const Sponsor = (): JSX.Element => {
   return (
     <div>
-        <div className="relative items-center overflow-hidden">
-            {/* <div className= 'flex flex-col font-[Excalibur-Nouveau] text-center text-white text-[40px] '>
-              SPONSORS
-            </div> */}
-            
-            <div className="relative mt-[200px]">
-                <Image src={kipas}/>
-            </div>
-            <div className='absolute top-0'>
-              <div className= 'flex flex-col font-[Excalibur-Nouveau] text-center text-white text-[40px] mt-[150px]'>
-                MEDIA PARTNERS
-              </div>
-              <div className='flex xl:mt-[70px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-                <div className='relative w-1/3 flex flex-col xl:ml-[100px] ml-[20px] sm:ml-[30px] md:ml-[50px] lg:ml-[70px]'>
-                    <Image src = {acarakita}/>
-                </div>
-                <div className='relative w-1/3 flex flex-col xl:mx-[50px] mx-[10px] sm:mx-[15px] md:mx-[25px] lg:mx-[35px]'>
-                    <Image src = {skillUp}/>
-                </div>
-                <div className='relative w-1/3 flex flex-col xl:mr-[100px] mr-[20px] sm:mr-[30px] md:mr-[50px] lg:mr-[70px]'>
-                    <Image src = {aisec}/>
-                </div>
-              </div>
-              <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-                <div className='relative w-1/3 flex flex-col xl:ml-[100px] ml-[20px] sm:ml-[30px] md:ml-[50px] lg:ml-[70px]'>
-                    <Image src = {tribun}/>
-                </div> 
-                <div className='relative w-1/3 flex flex-col xl:mx-[50px] mx-[10px] sm:mx-[15px] md:mx-[25px] lg:mx-[35px]'>
-                    <Image src = {inilah}/>
-                </div>
-                <div className='relative w-1/3 flex flex-col xl:mr-[100px] mr-[20px] sm:mr-[30px] md:mr-[50px] lg:mr-[70px]'>
-                    <Image src = {parah}/>
-                </div>
-              </div>
-              <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-                <div className='relative w-1/3 flex flex-col xl:ml-[100px] ml-[20px] sm:ml-[30px] md:ml-[50px] lg:ml-[70px]'>
-                
-                </div> 
-                <div className='relative w-1/3 flex flex-col xl:mx-[50px] mx-[10px] sm:mx-[15px] md:mx-[25px] lg:mx-[35px]'>
-                  <Image src = {infobandung}/>
-                </div>
-                <div className='relative w-1/3 flex flex-col xl:mr-[100px] mr-[20px] sm:mr-[30px] md:mr-[50px] lg:mr-[70px]'>
-                  
-                </div>
-              </div>
-              </div>
+      <div className='h-auto'>
+        <div className= 'flex flex-col font-[Excalibur-Nouveau] text-center text-white text-[40px] mt-[150px]'>
+          SPONSORS
         </div>
+        <div className='flex xl:mt-[70px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
+                  <Image src = {formula}/>
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
+                  <Image src = {fullo}/>
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
+                  <Image src = {macito}/>
+              </div>
+            </div>
+            <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
+                  <Image src = {mintz}/>
+              </div> 
+              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
+                  
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
+                  <Image src = {tango}/>
+              </div>
+            </div>
+      </div>
+        <div className="relative items-center overflow-hidden">         
+          <div className="relative mt-[100px]">
+            <Image src={kipas}/>
+          </div>
+          <div className='absolute top-0'>
+            <div className= 'flex flex-col font-[Excalibur-Nouveau] text-center text-white text-[40px] mt-[50px]'>
+              MEDIA PARTNERS
+            </div>
+            <div className='flex xl:mt-[70px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
+                  <Image src = {acarakita}/>
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
+                  <Image src = {skillUp}/>
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
+                  <Image src = {aisec}/>
+              </div>
+            </div>
+            <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
+                  <Image src = {tribun}/>
+              </div> 
+              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
+                  <Image src = {inilah}/>
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
+                  <Image src = {parah}/>
+              </div>
+            </div>
+            <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
+              
+              </div> 
+              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
+                <Image src = {infobandung}/>
+              </div>
+              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
+                
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
