f2085da3f53a44f1034dfeac5590a9f9b398a812
bewe <16521486@mahasiswa.itb.ac.id>
2023-04-02 14:51:52 +0700

    feat: add new sponsors

--- src/contexts/Homepage/Sponsor.tsx
+++ src/contexts/Homepage/Sponsor.tsx
@@ -6,6 +6,10 @@ import fullo from '@src/assets/images/sponsornmedpar/fullo-black-logo-1.svg'
 import macito from '@src/assets/images/sponsornmedpar/logo-macito-png-2.svg'
 import mintz from '@src/assets/images/sponsornmedpar/logo-mintz-soft-candy-1.svg'
 import tango from '@src/assets/images/sponsornmedpar/logo-tango-2021-01-1.svg'
+import BRI from '@src/assets/images/sponsornmedpar/BRI.svg'
+import BNI from '@src/assets/images/sponsornmedpar/BNI.svg'
+import ayam from '@src/assets/images/sponsornmedpar/ayaman.svg'
+import lps from '@src/assets/images/sponsornmedpar/lps.svg'
 
 import acarakita from '@src/assets/images/sponsornmedpar/acarakita-logo.svg'
 import skillUp from '@src/assets/images/sponsornmedpar/skillup-logo.svg'
@@ -23,70 +27,81 @@ const Sponsor = (): JSX.Element => {
           SPONSORS
         </div>
         <div className='flex xl:mt-[70px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
-                  <Image src = {formula}/>
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
-                  <Image src = {fullo}/>
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
-                  <Image src = {macito}/>
-              </div>
-            </div>
-            <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
-                  <Image src = {mintz}/>
-              </div> 
-              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
-                  
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
-                  <Image src = {tango}/>
-              </div>
-            </div>
+          <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px] items-center justify-center'>
+              <Image src = {formula}/>
+          </div>
+          <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px] items-center justify-center'>
+              <Image src = {fullo}/>
+          </div>
+          <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px] items-center justify-center'>
+              <Image src = {macito}/>
+          </div>
+        </div>
+        <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+          <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px] items-center justify-center'>
+              <Image src = {mintz}/>
+          </div> 
+          <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px] items-center justify-center'>
+            <Image src = {BRI}/>
+          </div>
+          <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px] items-center justify-center'>
+              <Image src = {tango}/>
+          </div>
+        </div>
+        <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+          <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px] items-center justify-center'>
+              <Image src={BNI} />
+          </div>
+          <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px] items-center justify-center'>
+            <Image src = {ayam}/>
+          </div>
+          <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px] items-center justify-center'>
+              <Image src = {lps}/>
+          </div>
+        </div>
       </div>
-        <div className="relative items-center overflow-hidden">         
-          <div className="relative mt-[100px]">
-            <Image src={kipas}/>
+      <div className="relative items-center overflow-hidden">         
+        <div className="relative mt-[150px]">
+          <Image src={kipas}/>
+        </div>
+        <div className='absolute top-0'>
+          <div className= 'flex flex-col font-[Excalibur-Nouveau] text-center text-white text-[40px] mt-[100px]'>
+            MEDIA PARTNERS
           </div>
-          <div className='absolute top-0'>
-            <div className= 'flex flex-col font-[Excalibur-Nouveau] text-center text-white text-[40px] mt-[50px]'>
-              MEDIA PARTNERS
+          <div className='flex xl:mt-[70px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+            <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px] items-center justify-center'>
+                <Image src = {acarakita}/>
             </div>
-            <div className='flex xl:mt-[70px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
-                  <Image src = {acarakita}/>
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
-                  <Image src = {skillUp}/>
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
-                  <Image src = {aisec}/>
-              </div>
+            <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px] items-center justify-center'>
+                <Image src = {skillUp}/>
             </div>
-            <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
-                  <Image src = {tribun}/>
-              </div> 
-              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
-                  <Image src = {inilah}/>
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
-                  <Image src = {parah}/>
-              </div>
+            <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px] items-center justify-center'>
+                <Image src = {aisec}/>
             </div>
-            <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
-              <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px]'>
+          </div>
+          <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+            <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px] items-center justify-center'>
+                <Image src = {tribun}/>
+            </div> 
+            <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px] items-center justify-center'>
+                <Image src = {inilah}/>
+            </div>
+            <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px] items-center justify-center'>
+                <Image src = {parah}/>
+            </div>
+          </div>
+          <div className='flex xl:mt-[50px] mt-[10px] sm:mt-[20px] md:mt-[30px] lg:mt-[50px]'>
+            <div className='relative w-1/3 flex flex-col xl:ml-[200px] ml-[20px] sm:ml-[50px] md:ml-[100px] lg:ml-[150px] items-center justify-center'>
+            
+            </div> 
+            <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px] items-center justify-center'>
+              <Image src = {infobandung}/>
+            </div>
+            <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px] items-center justify-center'>
               
-              </div> 
-              <div className='relative w-1/3 flex flex-col xl:mx-[100px] mx-[10px] sm:mx-[25px] md:mx-[50px] lg:mx-[75px]'>
-                <Image src = {infobandung}/>
-              </div>
-              <div className='relative w-1/3 flex flex-col xl:mr-[200px] mr-[20px] sm:mr-[50px] md:mr-[100px] lg:mr-[150px]'>
-                
-              </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   )
