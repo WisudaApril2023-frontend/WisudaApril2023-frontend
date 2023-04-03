0631b7b8a2a02a0c79a9fc2d477e884db165c971
unknown <aldeojr@gmail.com>
2023-04-04 03:33:07 +0700

    docs: TA Fair data wisudawan

--- src/contexts/Event/TAFair.tsx
+++ src/contexts/Event/TAFair.tsx
@@ -17,8 +17,8 @@ const cards = [
   {
     name: 'Maulidya Nur Sabhitayarn ',
     nim: '10218053',
-    prodi: 'Teknik Fisika',
-    fakultas: 'FTI',
+    prodi: 'Fisika',
+    fakultas: 'FMIPA',
     image: 'https://yt3.googleusercontent.com/DNfVMdufPYu-o0Bf_AVC_wuNHGE-Yuf7YczMyM1dVXmM6hUqMx13xVwwuJZFHxX_Nl-5BtwR=s900-c-k-c0x00ffffff-no-rj'
   },
   {
@@ -33,6 +33,125 @@ const cards = [
     nim: '20320015',
     prodi: 'Astronomi',
     fakultas: 'FMIPA',
+    image: ''
+  },
+  {
+    name: 'Balya Elfata',
+    nim: '10217001',
+    prodi: 'Fisika',
+    fakultas: 'FMIPA',
+    image: ''
+  },
+  {
+    name: 'Shafira Nur Azizah Mulyana',
+    nim: '17018025',
+    prodi: 'Seni Rupa',
+    fakultas: 'FSRD',
+    image: ''
+  },
+  {
+    name: 'Ivan Bertrand Reynaldi Widagdo',
+    nim: '12318010',
+    prodi: 'Teknik Geofisika',
+    fakultas: 'FTTM',
+    image: ''
+  },
+  {
+    name: 'Annisa Salsabiila',
+    nim: '15817014',
+    prodi: 'Teknik dan Pengelolaan Sumber Daya Air',
+    fakultas: 'FTSL',
+    image: ''
+  },
+  {
+    name: 'Suci Nilasari Azis',
+    nim: '11618034',
+    prodi: 'Farmasi Klinik dan Komunitas',
+    fakultas: 'SF',
+    image: ''
+  },
+  {
+    name: 'Fahmi Faisal Habib Boedhi Wiarso',
+    nim: '13618115',
+    prodi: 'Teknik Dirgantara',
+    fakultas: 'FTMD',
+    image: ''
+  },
+  {
+    name: 'Samuel Julian Harjanto',
+    nim: '13118131',
+    prodi: 'Teknik Mesin',
+    fakultas: 'FTMD',
+    image: ''
+  },
+  {
+    name: 'Ahmad Toifur',
+    nim: '12318031',
+    prodi: 'Teknik Geofisika',
+    fakultas: 'FTTM',
+    image: ''
+  },
+  {
+    name: 'Radisya Daffa Rihaatul Aisy',
+    nim: '10518099',
+    prodi: 'Kimia',
+    fakultas: 'FMIPA',
+    image: ''
+  },
+  {
+    name: 'Faiq Ahmad Ramadhan',
+    nim: '13718048',
+    prodi: 'Teknik Material',
+    fakultas: 'FTMD',
+    image: ''
+  },
+  {
+    name: 'Hamidah Azzahra',
+    nim: '17018014',
+    prodi: 'Seni Rupa',
+    fakultas: 'FSRD',
+    image: ''
+  },
+  {
+    name: 'Fatiha Agyal Shahwiya',
+    nim: '10318003',
+    prodi: 'Astronomi',
+    fakultas: 'FMIPA',
+    image: ''
+  },
+  {
+    name: 'Mangambar Arumsari',
+    nim: '17018009',
+    prodi: 'Seni rupa',
+    fakultas: 'FSRD',
+    image: ''
+  },
+  {
+    name: 'Advan Dwi Prayuda',
+    nim: '15318061',
+    prodi: 'Teknik Lingkungan',
+    fakultas: 'FTSL',
+    image: ''
+  },
+  {
+    name: 'Aldy Nurhajri Sidi',
+    nim: '15018020',
+    prodi: 'Teknik Sipil',
+    fakultas: 'FTSL',
+    image: ''
+  },
+  {
+    name: 'Muhammad Abdurrahman Jayadita',
+    nim: '12318063',
+    prodi: 'Teknik Geofisika',
+    fakultas: 'FTTM',
+    image: ''
+  },
+  {
+    name: 'Muhammad Sultoni',
+    nim: '15018046',
+    prodi: 'Teknik Sipil',
+    fakultas: 'FTSL',
     image: ''
   }
 ]
