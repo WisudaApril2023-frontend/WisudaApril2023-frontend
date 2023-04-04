95ff9fbabe25b189a1a262a2e598a0b02866a5c0
bewe <16521486@mahasiswa.itb.ac.id>
2023-04-04 18:45:08 +0700

    feat: back button

--- src/components/Button/YellowButton1.tsx
+++ src/components/Button/YellowButton1.tsx
@@ -19,16 +19,12 @@ const YellowButton1 = (): JSX.Element => {
     setIsHovered(false)
   }
 
-  const handleClick = (): void => {
-    setIsClicked(true)
-    // void new Promise<void>((resolve) => {
-    //   setTimeout(() => {
-    //     setIsClicked(false)
-    //     router.push('/Event')
-    //     resolve()
-    //   }, 200)
-    // }).catch((error) => console.error(error))
-  }
+  // const handleClick = (): void => {
+  //   setIsClicked(true)
+  //   setTimeout(() => {
+  //     setIsClicked(false)
+  //   }, 200)
+  // }
 
   let buttonImage = yb1Default
   if (isClicked) {
@@ -38,41 +34,43 @@ const YellowButton1 = (): JSX.Element => {
   }
 
   return (
-    <div
-      onClick={ handleClick }
-      onMouseEnter={ handleMouseEnter }
-      onMouseLeave={ handleMouseLeave }
-      style={{
-        position: 'absolute',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        cursor: 'pointer',
-        userSelect: 'none'
-      }}
-    >
-      <Image src={ buttonImage } alt="Yellow Button 1" width={200} height={60} />
+    <a href='/Event'>
       <div
+        // onClick={ handleClick }
+        onMouseEnter={ handleMouseEnter }
+        onMouseLeave={ handleMouseLeave }
         style={{
           position: 'absolute',
-          width: '100px',
-          height: '100px',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          fontWeight: 500,
-          fontFamily: 'Excalibur-Nouveau',
-          fontStyle: 'normal',
-          fontSize: '20px',
-          lineHeight: '24px',
-          letterSpacing: '0.1em',
-          textTransform: 'capitalize',
-          color: '#FFE8C4'
+          cursor: 'pointer',
+          userSelect: 'none'
         }}
       >
-        Back
+        <Image src={ buttonImage } alt="Yellow Button 1" width={200} height={60} />
+        <div
+          style={{
+            position: 'absolute',
+            width: '100px',
+            height: '100px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontWeight: 500,
+            fontFamily: 'Excalibur-Nouveau',
+            fontStyle: 'normal',
+            fontSize: '20px',
+            lineHeight: '24px',
+            letterSpacing: '0.1em',
+            textTransform: 'capitalize',
+            color: '#FFE8C4'
+          }}
+        >
+          Back
+        </div>
       </div>
-    </div>
+    </a>
   )
 }
 
